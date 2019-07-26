c628831dddc373d5c3992d20e5d03abd8cb6adef
IntrovertiDeyq <38270650+IntrovertiDeyq@users.noreply.github.com>
2019-07-26 15:24:22 +0800

    Update index.js

--- src/core/instance/index.js
+++ src/core/instance/index.js
@@ -5,12 +5,15 @@ import { eventsMixin } from './events'
 import { lifecycleMixin } from './lifecycle'
 import { warn } from '../util/index'
 
+//Vue的构造函数
+//安全检查，使用new调用
 function Vue (options) {
   if (process.env.NODE_ENV !== 'production' &&
     !(this instanceof Vue)
   ) {
     warn('Vue is a constructor and should be called with the `new` keyword')
   }
+  //初始化生命周期
   this._init(options)
 }
 
