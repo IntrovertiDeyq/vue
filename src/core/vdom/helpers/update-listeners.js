9beceab7a3776bfcf0ccd515b4cfc4f4d8882257
IntrovertiDeyq <38270650+IntrovertiDeyq@users.noreply.github.com>
2019-08-01 10:57:11 +0800

    Update update-listeners.js

--- src/core/vdom/helpers/update-listeners.js
+++ src/core/vdom/helpers/update-listeners.js
@@ -11,6 +11,8 @@ import {
   isPlainObject
 } from 'shared/util'
 
+//vuejs模板中支持事件修饰符，例如capture,once,passive等，如果使用在模板编译时会转换成相应的修饰符加在事件名的前面
+//所以需要将事件修饰符解析出来(passive:&  once:~  capture:!)
 const normalizeEvent = cached((name: string): {
   name: string,
   once: boolean,
@@ -33,6 +35,8 @@ const normalizeEvent = cached((name: string): {
   }
 })
 
+//将父组件传递的参数依次添加到子组件的实例上
+//闭包实现，返回一个函数，执行函数时依次执行监听的回调函数
 export function createFnInvoker (fns: Function | Array<Function>, vm: ?Component): Function {
   function invoker () {
     const fns = invoker.fns
@@ -59,6 +63,7 @@ export function updateListeners (
   vm: Component
 ) {
   let name, def, cur, old, event
+  //循环遍历on，判断哪些事件在oldOn中不存在则注册
   for (name in on) {
     def = cur = on[name]
     old = oldOn[name]
@@ -69,14 +74,20 @@ export function updateListeners (
       event.params = def.params
     }
     if (isUndef(cur)) {
+      //on事件名对应的值如果为undefined或者null则在控制台触发警告
       process.env.NODE_ENV !== 'production' && warn(
         `Invalid handler for event "${event.name}": got ` + String(cur),
         vm
       )
     } else if (isUndef(old)) {
+      //事件名在oldIn中不存在则注册新事件
       if (isUndef(cur.fns)) {
+        //依次将父组件注册的事件添加到子组件的实例上
         cur = on[name] = createFnInvoker(cur, vm)
       }
+      //只执行一次
+      //createOnceHandler返回一个函数封装了执行函数，子组件实例的事件监听器保存该回调函数
+      //该函数执行时先执行原函数，之后再解绑该事件的监听器
       if (isTrue(event.once)) {
         cur = on[name] = createOnceHandler(event.name, cur, event.capture)
       }
@@ -86,6 +97,7 @@ export function updateListeners (
       on[name] = old
     }
   }
+  //循环遍历oldOn，在on中不存在的事件则解绑事件
   for (name in oldOn) {
     if (isUndef(on[name])) {
       event = normalizeEvent(name)
